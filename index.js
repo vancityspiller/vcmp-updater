f4ba2fbb674fc3e9ec78a8e8bfec9e9df3bc09cd
Spiller <xiwcoc@gmail.com>
2022-01-14 16:04:10 +0530

    Should have done this earlier

--- index.js
+++ index.js
@@ -3,98 +3,168 @@ const _config = {
     port: 8000
 }
 
+// ------------------------------------------------------- //
+
 const _versions = {
     "04rel006": "5FE83BB4"
 }
 
+// ======================================================= //
+
+const fs = require('fs');
 const server = require('http').createServer(function(req, res) {
 
     // only POST requests
-    if(req.method != 'POST') return;
+    if(req.method !== 'POST') return;
 
-    switch(req.url)
-    {
-        case '/check': {
+    var reqBody = '';
+    req.on('data', function(chunk) {
+        reqBody += chunk.toString();
+    });
 
-            var reqBody = '';
+    // ------------------------------------------------------- //
 
-            req.on('data', function(chunk) {
-                reqBody += chunk.toString();
+    req.on('end', function() {   
+        
+        // extract our data from the form-data (couldn't find a reliable parser :X)
+        const jsonStart = reqBody.indexOf('"json"'), jsonEnd = reqBody.lastIndexOf("}");
+
+        // no data provided?
+        if(jsonStart == -1 || jsonEnd == -1)
+        {
+            // return a bad request status
+            res.writeHead(400); 
+            res.end();
+
+            return;
+        }
+
+        // slice it out
+        const jsonData = JSON.parse(reqBody.slice(jsonStart + 9, jsonEnd + 1));
+
+        // ------------------------------------------------------- //
+
+        // request doesn't have a password field
+        if(!jsonData.hasOwnProperty("password")) {
+
+            // return a bad request status
+            res.writeHead(400); 
+            res.end();
+
+            return;
+        }
+
+        // updater password is set and doesn't match
+        if(_config.password != "" && jsonData.password != _config.password) {
+
+            // return an unauthorized status
+            res.writeHead(401); 
+            res.end();
+
+            return;
+        }
+
+        // ======================================================= //
+
+        if(req.url === '/check') {
+
+            // request doesn't have versions key
+            if(!jsonData.hasOwnProperty("versions")) {
+
+                // return a bad request status
+                res.writeHead(400); 
+                res.end();
+
+                return;
+            }
+
+            // ------------------------------------------------------- //
+            
+            // process versions
+            const reqVersions = Object.keys(jsonData.versions);
+            var newVersions = [];
+
+            reqVersions.forEach(v => {
+
+                // we don't have that version
+                if(!_versions.hasOwnProperty(v)) return;
+
+                // compare the build version
+                const v1 = parseInt(jsonData.versions[v], 16), v2 = parseInt(_versions[v], 16);
+                if(v1 < v2) newVersions.push(v);
+            });     
+            
+            // OK
+            res.writeHead(200);
+
+            // reduce it to single value
+            if(newVersions.length > 0) {
+
+                newVersions = newVersions.reduce((p, n) =>  p + "|" + n);
+                res.end(newVersions)
+            }
+            else res.end();
+        }
+
+        // ======================================================= //
+
+        else if (req.url === '/download') {
+
+            // request doesn't have version key
+            if(!jsonData.hasOwnProperty("version")) {
+
+                // return a bad request status
+                res.writeHead(400); 
+                res.end();
+
+                return;
+            }
+
+            // updater password is set and doesn't match
+            if(_config.password != "" && jsonData.password != _config.password) {
+
+                // return an unauthorized status
+                res.writeHead(401); 
+                res.end();
+
+                return;
+            }
+
+            // ------------------------------------------------------- //
+
+            if(!_versions.hasOwnProperty(jsonData.version)) {
+
+                // return 404
+                res.writeHead(404); 
+                res.end();
+
+                return;
+            }
+
+            const file = 'build' + _versions[jsonData.version] + '.7z';
+            const path = './builds/' + file;
+            const fileSize = fs.statSync(path).size;
+
+            res.writeHead(200, {
+                'Content-Description' : 'File Transfer',
+                'Content-Type' : 'application/octet-stream',
+                'Content-Disposition' : 'attachment; filename=' + file,
+                'Content-Length' : fileSize
             });
 
-            req.on('end', function() {   
-                
-                // extract our data from the form-data (couldn't find a reliable parser :X)
-                const jsonStart = reqBody.indexOf('"json"'), jsonEnd = reqBody.lastIndexOf("}");
-
-                // no data provided?
-                if(jsonStart == -1 || jsonEnd == -1)
-                {
-                    // return a bad request status
-                    res.writeHead(400); 
-                    res.end();
-
-                    return;
-                }
-
-                // slice it out
-                const jsonData = JSON.parse(reqBody.slice(jsonStart + 9, jsonEnd + 1)); 
-
-                // request doesn't match the format
-                if(!jsonData.hasOwnProperty("password") ||!jsonData.hasOwnProperty("versions"))
-                {
-                    // return a bad request status
-                    res.writeHead(400); 
-                    res.end();
-
-                    return;
-                }
-
-                // updater password is set and doesn't match
-                if(_config.password != "" && jsonData.password != _config.password)
-                {
-                    // return an unauthorized status
-                    res.writeHead(401); 
-                    res.end();
-
-                    return;
-                }
-                
-                // process versions
-                const reqVersions = Object.keys(jsonData.versions);
-                var newVersions = [];
-
-                reqVersions.forEach(v => {
-
-                    // we don't have that version
-                    if(!_versions.hasOwnProperty(v)) return;
-
-                    // compare the build version
-                    const v1 = parseInt(jsonData.versions[v], 16), v2 = parseInt(_versions[v], 16);
-                    if(v1 < v2) newVersions.push(v);
-                });     
-                
-                // OK
-                res.writeHead(200);
-
-                // reduce it to single value
-                if(newVersions.length > 0) {
-
-                    newVersions = newVersions.reduce((p, n) =>  p + "|" + n);
-                    res.end(newVersions)
-                }
-                else res.end();
-            });   
-                
-            break;
+            const readStream = fs.createReadStream(path);
+            readStream.pipe(res);
         }
 
-        case '/download':
-        {
+        // ======================================================= //
 
-            break;
+        else {
+
+            // return a not found
+            res.writeHead(404);
+            res.end();
         }
-    }
+    });
 });
 
 server.listen(_config.port);
